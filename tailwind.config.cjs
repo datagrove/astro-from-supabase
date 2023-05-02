48a5bba106b9ae1fcf40e5bd84c727f9e87e901b
Meagan <meagan.h.smith@gmail.com>
2023-05-02 12:17:36 -0500

    Fix tailwind config custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+// const colors = {
+// 	...defaultColors,
+// 	...{
+// 		"dgGreen": {
+// 			"500": "435E1C"
+// 		},
+// 		"accent": "812458",
+// 		"dgBlue": {
+// 			"500": "245881"
+// 		}
+// 	}
+// }
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/components/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', 
+		'.src/layouts/**/*.astro', 
+		'.src/pages/**/*.astro'
+	],
+	darkMode: "class",
 	theme: {
-		colors: {
-			"dgGreen": '435E1C',
-			'accent': '812458',
-			'dgBlue': '245881'
-		},
+		// "colors": colors,
 		extend: {
+			colors: {
+				dgGreen: "435E1C",
+				accent: "812458",
+				dgBlue: "245881"
+			},
 			gridTemplateColumns: {
 				'card': 'repeat(auto-fit, minmax(24ch, 1fr))'
-			}
+			},
+			fontFamily: {
+				sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+			},
 		}
 
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 }
