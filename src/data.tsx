7213db569513fb7005a48d3fadb6b838b21f62e0
Meagan <meagan.h.smith@gmail.com>
2023-05-03 11:18:16 -0500

    Attempt a dropdown link

---
+++ src/data.tsx
@@ -0,0 +1,21 @@
+export const headerData = {
+    links: [
+        {
+            text: "Home",
+            href: "/"
+        },
+        {
+            text: "About Us",
+            href: "/about"
+        },
+        {
+            text: "Solutions",
+            links: [
+                {
+                    text: "Homeschool",
+                    href: "/pawpaw"
+                }
+            ]
+        }
+    ]
+}
