68c367d226e4f628ce949283abb5a5181bb2b820
Meagan <meagan.h.smith@gmail.com>
2023-05-04 17:18:59 -0500

    Fix dropdown menu, add page content, add custom styles

--- src/data.ts
+++ src/data.ts
@@ -19,11 +19,11 @@ export const headerData = {
                     text: "Test another link",
                     href: "/"
                 }
-            ]
+            ],
         },
         {
             text: "Contact",
             href: "/contact"
-        }
+        },
     ]
 }
