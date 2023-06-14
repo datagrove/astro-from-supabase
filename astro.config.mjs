60ab9b2475194c421474aba6c65ca1cea598923f
r-southworth <rachael.southworth@datagrove.com>
2023-06-14 17:51:52 -0400

    test hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-  ]
-});
+  integrations: [tailwind(), react()],
+  adapter: cloudflare(),
+  output: 'hybrid',
+  });
