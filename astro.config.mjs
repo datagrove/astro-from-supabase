4da8137a166bcacd9c004a6b265bbc17c26ba551
r-southworth <rachael.southworth@datagrove.com>
2023-06-16 11:26:27 -0400

    standard mdx working remote MDX not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   adapter: cloudflare(),
-  output: 'hybrid',
-  });
+  output: 'hybrid'
+});
