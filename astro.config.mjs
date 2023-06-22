95023d41d4be6c684dfb81d03e81302b4b361b5b
r-southworth <rachael.southworth@datagrove.com>
2023-06-22 14:31:02 -0400

    try to use eval, getting an error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), solidJs()],
   adapter: cloudflare(),
   output: 'hybrid'
 });
